854d497de467f814def6b045b06ea23ea0fc4644
Paul Joannon <paul.joannon@gmail.com>
2014-06-30 10:56:24 +0200

    openbox shortcuts

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -2,3 +2,7 @@
 (sleep 0.1 && tint2) &
 (sleep 0.1 && lxterminal) &
 (sleep 0.1 && enGIF) &
+(sleep 0.16 && thunderbird) &
+(sleep 0.18 && pidgin) &
+(sleep 0.20 && firefox) &
+(sleep 0.22 && skype) &
