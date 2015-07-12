d07fec40bfebfa9d0a3727f66cb8b91cfc32ba10
Paul Joannon <hello@pauljoannon.com>
2015-07-12 15:18:26 +0200

    remove old files

--- openbox/autostart.sh
+++
@@ -1,9 +0,0 @@
-(sleep 0.08 && nitrogen --restore) &
-(sleep 0.10 && tint2) &
-(sleep 0.12 && lxterminal) &
-(sleep 0.14 && enGIF) &
-(sleep 0.16 && thunderbird) &
-(sleep 0.18 && pidgin) &
-(sleep 0.20 && firefox) &
-(sleep 0.22 && skype) &
-(sleep 0.24 && keepassx) &
--- scripts/nextWallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ppid=$(cat "/home/paulloz/.config/wallpaper/pid")
-process=$(ps --ppid $ppid | grep "sleep")
-set -- $process
-pid=$1
-kill $pid
