38dc8bfa4ad3cd3bdc7d2fadad27202acf77b4b0
Paul Joannon <hello@pauljoannon.com>
2021-03-20 11:24:20 +0100

    posh prompt (with git-posh)

---
+++ posh/profile.ps1
@@ -0,0 +1,31 @@
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+Import-Module posh-git
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+$GitPromptSettings.BeforePath = '╭─['
+$GitPromptSettings.BeforePath.ForegroundColor = [ConsoleColor]::Yellow
+$GitPromptSettings.AfterPath = ']'
+$GitPromptSettings.AfterPath.ForegroundColor = [ConsoleColor]::Yellow
+$GitPromptSettings.PathStatusSeparator = '-'
+$GitPromptSettings.PathStatusSeparator.ForegroundColor = [ConsoleColor]::Yellow
+$GitPromptSettings.DefaultPromptSuffix = '╰─ '
+$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Yellow
+$GitPromptSettings.LocalWorkingStatusSymbol.ForegroundColor = [System.ConsoleColor]::Magenta
+$GitPromptSettings.LocalStagedStatusSymbol.ForegroundColor = [System.ConsoleColor]::Magenta
+
+function prompt {
+    $identity  = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $identity 
+
+    $prompt = ""
+
+    if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        $prompt += Write-Prompt "[" -ForegroundColor ([ConsoleColor]::Yellow)
+        $prompt += Write-Prompt "Admin" -ForegroundColor ([ConsoleColor]::Red)
+        $prompt += Write-Prompt "]-" -ForegroundColor ([ConsoleColor]::Yellow)
+    }
+
+    $prompt += & $GitPromptScriptBlock
+
+    if ($prompt) { "$prompt" } else { " " }
+}
